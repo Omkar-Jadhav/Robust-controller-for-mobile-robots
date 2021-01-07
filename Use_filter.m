a172821f778e61948008cb07184667eca9a44e13
Omkar-Jadhav <omkar.jadhav00@gmail.com>
2021-01-07 21:58:22 +0530

    Update Use_filter.m

--- Use_filter.m
+++ Use_filter.m
@@ -7,7 +7,7 @@ b = [1/4 1/4 1/4 1/4];
 x_vec=[x_vec,x_update]; % Updating I_tilde vec with new updated value
 x_vec=x_vec(:,end-3:end); %Taking the latest 4 values only
 
-y = filter(b,a,x_vec);
+y = filter(b,a,x_vec,[],2);
 
 x_filtered=y(:,end); %Using the latest filtered value
 
